31b1eaa045a8a612f8d1af03439c434ba2f6fa62
Mario vizcaino <mario.vizcainos@gmail.com>
2018-02-06 16:56:44 -0500

    add users

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = { users: [] }
+  }
+  handleSubmit (event) {
+    event.preventDefault()
+    const form = event.target
+    const data = new FormData(form)
+    const newUser = {
+      firstName: data.get('first-name'),
+      lastName: data.get('last-name')
+    }
+    this.setState({
+      users: this.state.users.concat(newUser)
+    })
+    form.reset()
+  }
+  render () {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-sm-offset-3">
-            <form>
-              <div className="form-group">
-                <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-6 col-sm-offset-3'>
+            <form onSubmit={this.handleSubmit}>
+              <div className='form-group'>
+                <label htmlFor='first-name'>Nombre</label>
+                <input type='text' className='form-control' name='first-name' />
               </div>
 
-              <div className="form-group">
-                <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+              <div className='form-group'>
+                <label htmlFor='last-name'>Apellido</label>
+                <input type='text' className='form-control' name='last-name' />
               </div>
 
-              <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+              <div className='action'>
+                <button type='submit' className='btn btn-primary'>Agregar Invitado</button>
               </div>
             </form>
 
-            <table className="table bordered-table table-striped">
+            <table className='table bordered-table table-striped'>
               <thead>
                 <tr>
                   <th>Nombre</th>
@@ -30,7 +48,18 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.state.users.map((user, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>
+                        {user.firstName}
+                      </td>
+                      <td>
+                        {user.lastName}
+                      </td>
+                    </tr>
+                  )
+                })}
               </tbody>
             </table>
           </div>
