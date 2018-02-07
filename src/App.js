752e45d5054bf9d1eca1911a23c4f0b366f5c955
Mario vizcaino <mario.vizcainos@gmail.com>
2018-02-06 22:13:43 -0500

    update create users using react methods

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   constructor () {
     super()
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.state = { users: [] }
+    this.firstNameHandle = this.firstNameHandle.bind(this)
+    this.lastNameHandle = this.lastNameHandle.bind(this)
+    this.state = {users: [], user: {firstName: '', lastName: ''}}
   }
   handleSubmit (event) {
     event.preventDefault()
-    const form = event.target
-    const data = new FormData(form)
-    const newUser = {
-      firstName: data.get('first-name'),
-      lastName: data.get('last-name')
-    }
     this.setState({
-      users: this.state.users.concat(newUser)
+      users: this.state.users.concat(this.state.user),
+      user: {firstName: '', lastName: ''}
     })
-    form.reset()
+    event.target.reset()
+  }
+  firstNameHandle (event) {
+    this.setState({
+      user: {firstName: event.target.value, lastName: this.state.user.lastName}
+    })
+  }
+  lastNameHandle (event) {
+    this.setState({
+      user: {firstName: this.state.user.firstName, lastName: event.target.value}
+    })
   }
   render () {
     return (
@@ -27,12 +34,12 @@ class App extends Component {
             <form onSubmit={this.handleSubmit}>
               <div className='form-group'>
                 <label htmlFor='first-name'>Nombre</label>
-                <input type='text' className='form-control' name='first-name' />
+                <input type='text' onChange={this.firstNameHandle} className='form-control' name='first-name' />
               </div>
 
               <div className='form-group'>
                 <label htmlFor='last-name'>Apellido</label>
-                <input type='text' className='form-control' name='last-name' />
+                <input type='text' onChange={this.lastNameHandle} className='form-control' name='last-name' />
               </div>
 
               <div className='action'>
